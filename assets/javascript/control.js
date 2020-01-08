a64b0e75fb7a7087b4786d4bf1c3d1cf33f2594d
Fung <koufongso@gmail.com>
2020-01-08 02:03:27 -0800

    added gif api function to the class tag div

--- assets/javascript/control.js
+++ assets/javascript/control.js
@@ -11,14 +11,32 @@ $(document).on("click", ".btn-add", function (event) {
 
     // create and display the tag
     var newTag = $('<div>').addClass('tag');
-    newTag.html(`
-        <span class="btn-close"> x </span> ${content} 
-    `);
+    newTag.html(`<span class="btn-close">x</span> ${content}`);
 
     $('#display-tag').append(newTag);
 });
 
 
-$(document).on("click",".btn-close",function(){
+$(document).on("click", ".btn-close", function () {
     $(this).closest('.tag').remove();
-})
+});
+
+
+$(document).on("click", ".tag", function () {
+    var key = "JRvrNMtu2KeupmBlZMUIUYAwxz2ugM87";
+    var content = $(this).text().slice(2);
+    var queryURL = `http://api.giphy.com/v1/gifs/search?q=${content}&api_key=${key}`
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var data = response.data;
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var newDiv = $('<div>').html(`<img src=${data[i].images.fixed_height.url}>`);
+            $('#display-gif').prepend(newDiv);
+        }
+
+    });
+});
