ae083cb326cf7e196baa921e2e79f7a8197d5c5f
Fung <koufongso@gmail.com>
2020-01-08 13:08:10 -0800

    prevent adding empty tag

--- assets/javascript/control.js
+++ assets/javascript/control.js
@@ -4,15 +4,18 @@ $(document).on("click", ".btn-add", function (event) {
     event.preventDefault();
     var content = $('#tag-content').val();
     console.log(content);
-    $('#tag-content').val("");
-    tagList.push(content);
-    // create and display the tag
-    var newTag = $('<div>').addClass('tag');
-    newTag.html(`<span class="btn-close">&#10005</span> <span class="tag-name">${content}</span>`);
-    $('#display-tag').append(newTag);
+    $('#tag-content').val(""); // clear the input bar
+
+    // only perfrom action if the input is not "empty"
+    if(content!=""){
+        tagList.push(content);
+        // create and display the tag
+        var newTag = $('<div>').addClass('tag');
+        newTag.html(`<span class="btn-close">&#10005</span> <span class="tag-name">${content}</span>`);
+        $('#display-tag').append(newTag);
+    }
 });
 
-var a;
 $(document).on("click", ".btn-close", function () {
     tagList.splice(tagList.indexOf($(this).siblings().text()),1); // remove the tag from the array
     $(this).parent('.tag').remove();
